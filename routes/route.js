02dc1a2017ee506b010a2ad450959436b94a7fae
Alex Antoine <antoinealex1234@gmail.com>
2023-08-25 16:13:38 -0400

    added auth middle to routes

--- routes/route.js
+++ routes/route.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 //controller methods
 const {getLoginPage,getSignupPage, registerUser,loginUser, getDashboard} = require('../controller/user');
 
 router.route('/').get(getLoginPage).post(loginUser);
+router.route('/login').get(getLoginPage).post(loginUser);
 router.route('/signup').get(getSignupPage,).post(registerUser);
-router.route('/user/dashboard').get(getDashboard)
+router.route('/user/dashboard').get(auth,getDashboard)
 
 module.exports = router;
