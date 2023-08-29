fe3a6fc02fa09c4a0cdcf6b02327bde5d1395619
Alex Antoine <antoinealex1234@gmail.com>
2023-08-29 10:27:58 -0400

    added new routes

--- routes/route.js
+++ routes/route.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 
 //controller methods
-const {getLoginPage,getSignupPage, registerUser,loginUser, getDashboard} = require('../controller/user');
+const {getLoginPage,getSignupPage, registerUser,loginUser, getDashboard, logOut, addUser, getRegisterPage} = require('../controller/user');
 
 router.route('/').get(getLoginPage).post(loginUser);
 router.route('/login').get(getLoginPage).post(loginUser);
 router.route('/signup').get(getSignupPage,).post(registerUser);
-router.route('/user/dashboard').get(auth,getDashboard)
+router.route('/register').get(auth,getRegisterPage).post(auth,addUser);
+router.route('/dashboard').get(auth,getDashboard)
+router.route('/logout').get(logOut)
 
 module.exports = router;
