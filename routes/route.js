411ade45929e97b628a71b33457cf09e3575a327
Alex Antoine <antoinealex1234@gmail.com>
2023-08-29 13:55:56 -0400

    added all users route

--- routes/route.js
+++ routes/route.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 
 //controller methods
-const {getLoginPage,getSignupPage, registerUser,loginUser, getDashboard, logOut, addUser, getRegisterPage} = require('../controller/user');
+const {getLoginPage,getSignupPage, registerUser,loginUser, getDashboard, logOut, addUser, getRegisterPage, getAllusers} = require('../controller/user');
 
 router.route('/').get(getLoginPage).post(loginUser);
 router.route('/login').get(getLoginPage).post(loginUser);
 router.route('/signup').get(getSignupPage,).post(registerUser);
 router.route('/register').get(auth,getRegisterPage).post(auth,addUser);
+router.route('/users/all').get(auth,getAllusers)
 router.route('/dashboard').get(auth,getDashboard)
 router.route('/logout').get(logOut)
 
