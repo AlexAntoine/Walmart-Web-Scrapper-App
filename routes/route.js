7f7bf0b8513a619add57757141cc2f6dac295848
Alex Antoine <antoinealex1234@gmail.com>
2023-09-06 13:08:15 -0400

    added update and delete routs

--- routes/route.js
+++ routes/route.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 
 //controller methods
-const {getLoginPage,getSignupPage, registerUser,loginUser, getDashboard, logOut, addUser, getRegisterPage, getAllusers} = require('../controller/user');
+const {getLoginPage,getEditUserPage, getSignupPage, registerUser,loginUser, getDashboard, logOut, addUser, getRegisterPage, getAllusers, updateUser, deleteUser} = require('../controller/user');
 
 router.route('/').get(getLoginPage).post(loginUser);
 router.route('/login').get(getLoginPage).post(loginUser);
 router.route('/signup').get(getSignupPage,).post(registerUser);
 router.route('/register').get(auth,getRegisterPage).post(auth,addUser);
-router.route('/users/all').get(auth,getAllusers)
+router.route('/users/all').get(auth,getAllusers);
+router.route('/delete/user/:id').delete(auth, deleteUser);
+router.route('/edit/:id').get(auth,getEditUserPage).put(updateUser)
 router.route('/dashboard').get(auth,getDashboard)
 router.route('/logout').get(logOut)
 
