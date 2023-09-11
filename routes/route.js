3a98d7be1d681801d899d1a86f79d6a7fd734694
Alex Antoine <antoinealex1234@gmail.com>
2023-09-11 09:46:46 -0400

    added new route

--- routes/route.js
+++ routes/route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 
 //controller methods
-const {getLoginPage,getEditUserPage, getSignupPage, registerUser,loginUser, getDashboard, logOut, addUser, getRegisterPage, getAllusers, updateUser, deleteUser, getChangePasswordPage} = require('../controller/user');
+const {getLoginPage,getEditUserPage, getSignupPage, registerUser,loginUser, getDashboard, logOut, addUser, getRegisterPage, getAllusers, updateUser, deleteUser,changePassword, getChangePasswordPage} = require('../controller/user');
 
 router.route('/').get(getLoginPage).post(loginUser);
 router.route('/login').get(getLoginPage).post(loginUser);
@@ -13,7 +13,7 @@ router.route('/users/all').get(auth,getAllusers);
 router.route('/delete/user/:id').delete(auth, deleteUser);
 router.route('/edit/:id').get(auth,getEditUserPage).put(updateUser)
 router.route('/dashboard').get(auth,getDashboard);
-router.route('/password/change').get(auth,getChangePasswordPage);
+router.route('/password/change').get(auth,getChangePasswordPage).post(auth,changePassword);
 router.route('/logout').get(logOut)
 
 module.exports = router;
