bcfaeda82cba67586d31daa7bb4338afac80349b
Alex Antoine <antoinealex1234@gmail.com>
2023-09-21 13:12:09 -0400

    route for deleting items

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const auth = require('../middleware/auth');
-const {getNewProductPage, saveNewProduct, getSearchPage, getInstockPage, deleteProduct} = require('../controller/admin');
+const {getNewProductPage, saveNewProduct, getSearchPage, getInstockPage, deleteProduct,priceChanged, deleteProdPriceChange} = require('../controller/admin');
 
 const router = express.Router();
 
@@ -10,4 +10,6 @@ router.route('/product/search').get(auth,getSearchPage);
 router.route('/delete/product/:id').delete(auth, deleteProduct);
 router.route('/products/instock').get(auth,getInstockPage);
 
+router.route('/products/pricechanged').get(auth,priceChanged).delete();
+router.route('/products/pricechanged/:id').delete(auth,deleteProdPriceChange);
 module.exports = router;
