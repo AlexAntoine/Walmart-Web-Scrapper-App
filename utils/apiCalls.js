490b38b32efccdcf919bc9b9c3172b7602ebbb5a
Alex Antoine <antoinealex1234@gmail.com>
2023-09-21 13:11:20 -0400

    create file for api calls

--- utils/apiCalls.js
+++ utils/apiCalls.js
@@ -1,7 +1,34 @@
-const User = require('../model/user');
- 
-exports.findUser = async(id)=>{
+const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
 
-    const user = await User.findById(id);
-   return user;
+exports.samsClubApiCall = async(url, page)=>{
+
+    let price = '';
+    try {
+        await page.goto(url,{waitUntil:'load', timeout:0});
+
+        const html = await page.evaluate(()=> document.body.innerHTML);
+
+        const $ = await cheerio.load(html);
+
+        const title = $('#main > div > div > div.sc-pc-large-desktop-product-card > div > div.sc-pc-large-desktop-layout-columns > div.sc-pc-large-desktop-layout-content > div.sc-pc-title-full-desktop > h1').text();
+        const dollar = $('#main > div > div > div.sc-pc-large-desktop-product-card > div > div.sc-pc-large-desktop-layout-columns > div.sc-pc-large-desktop-layout-content > div.sc-pc-channel-price > div > span.sc-price > span > span.Price-characteristic').text();
+        const cents = $('#main > div > div > div.sc-pc-large-desktop-product-card > div > div.sc-pc-large-desktop-layout-columns > div.sc-pc-large-desktop-layout-content > div.sc-pc-channel-price > div > span.sc-price > span > span.Price-mantissa').text();
+        const sku = $('#main > div > div > div.sc-pc-large-desktop-product-card > div > div.sc-pc-large-desktop-layout-columns > div.sc-pc-large-desktop-layout-content > div.sc-pc-title-full-desktop > div > div > div.sc-product-header-item-number').text();
+        const stock = 'In Stock';
+        price = `${dollar}.${cents}`
+
+        console.log('line 54: ',title);
+        return {
+            title,
+            price,
+            url,
+            sku,
+            stock
+        }
+    } catch (error) {
+        console.log(error);
+
+    
+    }
 }
